e487c8a9724402e84ef998785ae4903bfc9e57c1
Greeley Dawson <hgd4th@gmail.com>
2014-07-18 15:27:22 -0400

    Added delete button, does not work yet

--- app/scripts/postView.js
+++ app/scripts/postView.js
@@ -2,12 +2,13 @@ var PostSingle = Backbone.View.extend ({
 	el: '#blog',
 
 	events: {
-		'click h2 a': 'viewPost'
+		'click h2 a': 'viewPost',
+    'click .delete' : 'deletePost'
 	},
 
   initialize: function(attributes){
-	this.options = attributes;
-	this.render();
+  	this.options = attributes;
+  	this.render();
 	},
 
   render: function (options){
@@ -22,5 +23,16 @@ var PostSingle = Backbone.View.extend ({
   	event.stopPropagation();
   	var postID = $(event.target).attr('id');
   	window.name_router.navigate('#post/'+id, {trigger:true});
+  },
+
+  deletePost: function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    // Confirmation dialogue
+    if (window.confirm("Are you sure you want to delete this post?")) {
+      this.options.destroy({success: function () {
+        window.PostRouter.navigate('', { trigger: true }); // Route home
+      }});
+    }
   }
 });
